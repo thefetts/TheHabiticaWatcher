938c2d9a647a54f251321a9b8dc0d28f786a0cee
Jordan Fallon <thefetts@gmail.com>
2017-04-04 15:10:47 -0600

    Use port from environment if present

--- index.js
+++ index.js
@@ -13,4 +13,7 @@ app.get('/', (req, res, next) => {
   res.end();
 });
 
-app.listen(8080);
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`listening on ${port}`)
+});
