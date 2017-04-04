0e8fa4db0b60f8745579ad7648c411f95dbd3db6
Jordan Fallon <thefetts@gmail.com>
2017-04-04 14:07:12 -0600

    wip: logging out what the webhook receives to see if it works!

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+let bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.post('/webhook', (req, res, next) => {
+  console.log(req.body);
+  res.end();
+});
+
+app.listen(8080);
