6c4a65762ce985070a6acc2d6fa61c7bfd537352
Jordan Fallon <thefetts@gmail.com>
2017-04-04 16:27:10 -0600

    Add habitica client and some logging

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+// Habitica stuff
+const Habitica = require('habitica');
+const habitica = new Habitica({
+  id: 'ba64b1af-4cab-4e78-9a7a-f187c19e33da',
+  apiToken: '5219ca15-ca8f-41fe-a1c2-5d1b7b385ec8'
+});
+
+// habitica.post('/user/webhook', {url:'https://the-habitica-watcher.herokuapp.com/webhook', label: 'test hook'}).then(res => {
+//   console.log(res);
+// }).catch(err => {
+//   console.log(err);
+// });
+
+let userName = '';
+habitica.get('/user').then(res => {
+  userName = res.data.profile.name;
+  console.log(userName);
+});
+
+
+// Express stuff
 const express = require('express');
 const app = express();
 let bodyParser = require('body-parser');
@@ -5,7 +26,15 @@ let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 app.post('/webhook', (req, res, next) => {
-  console.log(req.body);
+  console.log('*********************************************');
+  console.log(req.body.task);
+  console.log('*********************************************');
+  console.log(req.body.user);
+  console.log('*********************************************');
+  const task = req.body.task;
+  if (task.type === 'todo' && task.completed) {
+    console.log(`${task.text} completed by ${userName}!`);
+  }
   res.end();
 });
 
